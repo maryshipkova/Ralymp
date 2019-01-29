0a8725f62b793bd333d27d7870c1791c38d72432
Fredi Fowler <inredikawb@gmail.com>
2019-01-30 01:59:52 +0300

    Add simple data parser from excel

---
+++ Ralymp.ExcelParser/ParticipationRow.cs
@@ -0,0 +1,12 @@
+﻿namespace Ralymp.ExcelParser
+{
+    public class ParticipationRow
+    {
+        public string StudentName { get; set; }
+        public string School { get; set; }
+        public int Form { get; set; }
+        public string Teacher { get; set; }
+        public string Subject { get; set; }
+        public int Place { get; set; }
+    }
+}
---
+++ Ralymp.ExcelParser/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ExcelDataReader;
+using OfficeOpenXml;
+
+namespace Ralymp.ExcelParser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            const string filePath = "winner-list-16.xlsx";
+            List<ParticipationRow> result = new List<ParticipationRow>();
+
+            using (FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    //Skip first row
+                    reader.Read();
+
+                    while (reader.Read())
+                    {
+                        result.Add(Convertor(reader));
+                    }
+                }
+            }
+
+
+        }
+
+        public static ParticipationRow Convertor(IExcelDataReader reader)
+        {
+            return new ParticipationRow
+            {
+                StudentName = reader.GetString(0),
+                School = reader[1].ToString(),
+                Form = int.Parse(reader[2].ToString()),
+                Teacher = reader.GetString(3),
+                Subject = reader.GetString(4),
+                Place = int.Parse(reader[5].ToString())
+            };
+        }
+    }
+}
