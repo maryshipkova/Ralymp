773c8642b3ab6c8355218430f55db85877ad5676
Maria Shipkova <maryshipkova@gmail.com>
2019-01-29 17:23:28 +0300

    new header style

--- Ralymp/RalympClient/src/components/Header/Header.tsx
+++ Ralymp/RalympClient/src/components/Header/Header.tsx
@@ -11,44 +11,22 @@ export default class Header extends Component<Props, State> {
 
     render() {
         const {activeItem} = this.state;
+        const navItems = ['home', 'school', 'teacher', 'student', 'subject'];
+        const navColor = 'violet';
 
         return (
+            <Menu inverted>
+                {navItems.map(item => (
+                    <Link to={`/${item}`}>
+                        <Menu.Item
+                            name={item}
+                            active={activeItem === item}
+                            onClick={this.handleItemClick}
+                            color={navColor}
+                        />
+                    </Link>
+                ))}
 
-            <Menu pointing secondary>
-                <Link to='/home'>
-                    <Menu.Item
-                        name='home'
-                        active={activeItem === 'home'}
-                        onClick={this.handleItemClick}
-                    />
-                </Link>
-                <Link to='/school'>
-                    <Menu.Item
-                        name='school'
-                        active={activeItem === 'school'}
-                        onClick={this.handleItemClick}
-                    />
-                </Link>
-                <Link to='/teacher'>
-                    <Menu.Item
-                        name='teacher'
-                        active={activeItem === 'teacher'}
-                        onClick={this.handleItemClick}
-                    />
-                </Link>
-                <Link to='/student'><Menu.Item
-                    name='student'
-                    active={activeItem === 'student'}
-                    onClick={this.handleItemClick}
-                />
-                </Link>
-                <Link to='/subject'>
-                    <Menu.Item
-                        name='subject'
-                        active={activeItem === 'subject'}
-                        onClick={this.handleItemClick}
-                    />
-                </Link>
             </Menu>
         )
     }
