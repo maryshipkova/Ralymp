74a6b9d6ab31764975efb8231a170cc1d66ef178
Fredi Fowler <inredikawb@gmail.com>
2019-01-31 19:53:57 +0300

    Add IStudentProfileService, fix AB#11

---
+++ Ralymp/Services/StudentProfileService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Ralymp.Models.ResponseTypes;
+using Ralymp.Tools;
+
+namespace Ralymp.Services
+{
+    public class StudentProfileService : IStudentProfileService
+    {
+        public StudentProfileResponse Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StudentProfileResponse GetRandom()
+        {
+            StudentProfileResponse response = TemplateInstanceCreator.GetStudentProfileResponse();
+            return response;
+        }
+
+        public StudentProfileResponse FindBySurname(string surname)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
